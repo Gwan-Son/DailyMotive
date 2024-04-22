0ecca5810a43e9a72f6709821daf534889640f98
GwanSon <id147369@naver.com>
2024-04-22 14:56:44 +0900

    Initial commit

---
+++ DailyMotive/DailyMotiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMotiveApp.swift
+//  DailyMotive
+//
+//  Created by 심관혁 on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyMotiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
