47492f8b83e7fae407e5d8eda40e74d568257143
GwanSon <id147369@naver.com>
2024-05-19 18:14:45 +0900

    update 24.05.19

---
+++ DailyMotive/QuizView/QuizView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizView.swift
+//  DailyMotive
+//
+//  Created by 심관혁 on 5/22/24.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizView()
+}
